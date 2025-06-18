0941878c013acb78dab57b6cfae6dd31ea0f6886
Oksy <oksiagabekyan@gmail.com>
2025-06-18 14:25:00 +0400

    updated code

---
+++ haykong/src/about/about.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+import './about.css';
+import pic1 from '../assets/1pic.jpg';
+import pic2 from '../assets/2pic.jpg';
+import pic3 from '../assets/3pic.jpg';
--- haykong/src/homepage/homepage.jsx
+++ haykong/src/homepage/homepage.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './homepage.css';
-// import bgImage from '../assets/gpt.jpg';
-import bgImage from '../assets/gptt.jpg';
+import bgImage from '../assets/gpt.jpg';
+// import bgImage from '../assets/gptt.jpg';
 
 const GeneralPage = () => {
   return (
@@ -9,12 +9,12 @@ const GeneralPage = () => {
       <img src={bgImage} alt="AI background" className="naturalBgImage" />
 
       <div className="overlayText">
-        <h1>Empowering the Future with AI</h1>
-        <h2>Smart. Fast. Limitless.</h2>
+        <h1>Welcome to our website!</h1>
+        <h2>Here you can find the results of the 2024–2025 IT course program. <br /><br />
+        Information technology training courses for wounded and disabled soldiers, volunteers, as well as forcibly displaced persons from Artsakh</h2>
         <p>
-          Welcome to the forefront of innovation. Our solutions combine the power of Artificial Intelligence and
-          technology to accelerate your growth and transform how you work. Dive into a smarter world, where your data
-          works for you.
+          <b>The goal of the project :</b> <br />
+          Considering the difficult social situation resulting from the 44-day war and the forced displacement of our compatriots from Artsakh, which still poses problems today in education, general development, and the elimination of the consequences of the war, we decided to relaunch the above-mentioned program in order to confront the difficult challenges created in the country. In order to increase the educational level of wounded or disabled soldiers and our compatriots forcibly displaced from Artsakh and to support them in finding high-paying jobs, the following educational events were held:
         </p>
       </div>
     </div>
